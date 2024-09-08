9b2cbd158ad64f2009408b842d203dab35cd2a8c
maxim <maximyakovlev228@gmail.com>
2024-09-08 17:41:24 +0500

    feat: Add currency conversion microservice

---
+++ currency-conversion-sevice/src/main/java/com/microservices/currency_conversion_sevice/CurrencyConversionSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.currency_conversion_sevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionSeviceApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-sevice/src/test/java/com/microservices/currency_conversion_sevice/CurrencyConversionSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.currency_conversion_sevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
